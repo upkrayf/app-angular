63bbda7a99f6ca9316ef5a9b774886d5e25b8939
upkrayf <zewor00@gmail.com>
2026-03-07 16:59:34 +0300

    new comps

---
+++ src/app/components/services/product.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Product as ProductInterface } from '../models/product';
+import { MOCK_PRODUCTS } from '../data/products';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Product {
+  private products: ProductInterface[] = MOCK_PRODUCTS;
+  constructor() {}
+  getProducts(): ProductInterface[] {
+    return this.products;
+  }
+
+  getProductById(id: number): ProductInterface | undefined {
+    return this.products.find(product => product.id === id);
+  }
+  
+}
